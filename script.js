425535925e30942c5086cf4968b442fa0fd7d6c9
Francisco Aguirre <franmaguirre@gmail.com>
2023-05-02 22:38:10 -0300

    update img

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ container.appendChild(renderer.domElement);
 
 // Load the panoramic image and create a texture
 const loader = new THREE.TextureLoader();
-const texture = loader.load('snowy.jpg');
+const texture = loader.load('realistic_hiking.jpg');
 
 // Create a spherical geometry and map the texture to it
 const geometry = new THREE.SphereGeometry(500, 60, 40);
